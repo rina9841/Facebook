b8e0afa069a5cc42f1469c112525f3a8507bc7be
rina9841 <pandyrina@gmail.com>
2022-02-23 12:56:22 -0500

    adding maven project along with pom.xml and java files

---
+++ src/test/java/com/Facebook/Facebook/FacebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Facebook.Facebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FacebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
